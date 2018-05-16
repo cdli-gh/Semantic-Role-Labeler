d9791b1f8c3a7ffa4b0f6e5a542381b2c0e49742
Bakhtiyar Syed <baksoverflow@gmail.com>
2018-05-16 23:37:03 +0530

    Created script for running Senna for the english source data

---
+++ scripts/srl_english.sh
@@ -0,0 +1,34 @@
+############################################################
+#
+# Usage : [./srl_english.py] [path_to_senna_dir] [input_file] [output_file]
+#
+############################################################
+
+#Function to get the absolute filepath
+function absolute_path() {
+  filename=$1
+  parentdir=$(dirname "${filename}")
+
+  if [ -d "${filename}" ]; then
+      echo "$(cd "${filename}" && pwd)"
+  elif [ -d "${parentdir}" ]; then
+    echo "$(cd "${parentdir}" && pwd)/$(basename "${filename}")"
+  fi
+}
+
+#Exit program if no. of arguments are not equal to 3
+if [[ $# -ne 3 ]]
+then
+	echo "Usage: ./srl_english.sh [path_to_senna_dir] [input_file] [output_file]"
+	exit
+fi
+
+#Store the input file in a variable
+INPUT_FILE=$(absolute_path $2)
+OUTPUT_FILE=$(absolute_path $3)
+
+#RUN THE SCRIPT FOR SENNA
+echo "Getting SRL tags..."
+cd $1;
+./senna -srl < $INPUT_FILE > $OUTPUT_FILE;
+echo "Done!"
