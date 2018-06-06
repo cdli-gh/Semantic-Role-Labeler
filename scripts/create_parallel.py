440993094374447b7dc4710fdb16eb8e08f8d1c9
Bakhtiyar Syed <baksoverflow@gmail.com>
2018-06-07 00:42:52 +0530

    Update parallel creation file

--- scripts/create_parallel.py
+++ scripts/create_parallel.py
@@ -19,18 +19,29 @@ for eng_file in eng_list:
 	contents_eng = [] # Store eng lines for a particular file
 	contents_sum = []
 
+	sum_dict = {}
 	print (sum_file)
 	print (eng_file)
 
 	with codecs.open(ENG_DIR+eng_file, 'r', encoding='utf-8') as e, codecs.open(SUM_DIR+sum_file, 'r', encoding='utf-8') as s:
 		next(e)  # Skip the first line
 		next(s)
+
+		for line in s:
+			line = line.split(',') # Split by comma for Sumerian
+
+			if len(line) < 4: #For broken lines in the Sumerian script
+				continue
+
+			sum_dict[line[3]] = line[4].strip() #Create a dictionary mapping of lines-strings in Sumerian
+
+
 		for line in e:
 			line = line.split('\t')
 
 
 			#Boundary conditions
-			if len(line) < 4 or '-' not in line[3]:
+			if len(line) < 4:
 				bad_files.append(eng_file)
 				continue
 			try:
@@ -38,47 +49,93 @@ for eng_file in eng_list:
 				c = int(line[3].split('-')[1])
 			except:
 				continue
-			
+			'''
 			if len(line[3].split('-'))==2:
 				line_ranges.append(int(line[3].split('-')[0]))
 				line_ranges.append(int(line[3].split('-')[1]))
 			else:
 				line_ranges.append(int(line[3]))
+			'''
 
+			line_ranges.append(line[3])
 
 			if (line[4]==''):
 				print ("EMPTY : ", eng_file)
+				continue
 			contents_eng.append(line[4])
 
 
-		
+		print (line_ranges)
 		i=0
 		temp_string = ''
+
+		for item in line_ranges:
+
+			# Assuming coherence now, no alpha chars. in b/w etc.
+
+			if len(item.split('-')) == 2:
+				start = int(item.split('-')[0])
+				end = int(item.split('-')[1])
+			elif len(item.split('-') == 1):
+				start = int(item)
+				end = int(item)
+			for x in range(start, end+1):
+				if str(x) in sum_dict:
+					temp_string = temp_string + sum_dict[str(x)].strip() + ' '
+
+
+			contents_sum.append(temp_string)
+			temp_string = ''
+
+
+		'''
 		for line in s:
 			line = line.split(',')
 
 			#Boundary condition if line_ranges ends
-			if (i+1) >= len(line_ranges):
-				temp_string = temp_string + line[4]
+			if i >= len(line_ranges):
+				temp_string = temp_string + line[4].strip()
 				contents_sum.append(temp_string)
 				temp_string = ''
 				continue
 
+			
 			# Error handling
 			try:
 				a = int(line[3])
 			except:
+				print ("bad file")
 				bad_files.append(eng_file)
 				continue
+			
+
+			if (line_ranges[i][0].isalpha()):
+				break #this is for sections now, modify later.
+
+
+			if (len(line_ranges.split('-') == 2):
+
+
+
+
+
+
+
+
+
+
+
 		
-			if int(line[3]) in list(range(line_ranges[i]+1, line_ranges[i+1]+1)):
-				temp_string = temp_string + line[4] + ' '
+			if int(line[3]) in list(range(line_ranges[i], line_ranges[i+1]+1)):
+
+				temp_string = temp_string + line[4].strip() + ' '
+				#print (temp_string)
 				continue
 			temp_string = temp_string.strip()
 			contents_sum.append(temp_string)
 			temp_string = ''
 			i+=1
-
+			'''
 		#print (contents_eng)
 		#print (contents_sum)
 		if eng_file not in bad_files:
