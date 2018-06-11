896c40743eec925c403979b1db6096f8e978d8be
Bakhtiyar Syed <baksoverflow@gmail.com>
2018-06-11 17:17:06 +0530

    Minor change

--- scripts/create_parallel.py
+++ scripts/create_parallel.py
@@ -1,3 +1,11 @@
+############################################################
+# Script for creating parallel aligned data from the existing XML parsed data.
+
+# Pre-requisites: - 
+
+# Usage : python create_parallel.py [../inputs/processed-etcsl-sumerian/] [../inputs/processed-etcsl-eng/]
+############################################################
+
 import sys, os
 import numbers, codecs
 
