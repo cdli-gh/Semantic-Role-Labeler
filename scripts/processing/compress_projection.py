a5f61ba1272d35b4ba973f180266685f0754a0fb
Bakhtiyar <baksoverflow@gmail.com>
2018-06-28 21:33:45 +0530

    Compressed projection script added

--- scripts/processing/compress_projection.py
+++ scripts/processing/compress_projection.py
@@ -3,14 +3,16 @@
 
 # Pre-requisites: - 
 
-# Usage : python3 compres_projection.py SUM_PROJECTED_FILE
+# Usage : python3 compress_projection.py SUM_PROJECTED_FILE
 ############################################################
 
 import sys, os
+import copy # for deepcopy
 import collections # for OrderedDict
 
 projected_list = []
 
+SUM_PROJECTED_FILE = sys.argv[1]
 with open(SUM_PROJECTED_FILE, 'r') as f:
 	m_line_no = 1
 
@@ -27,72 +29,54 @@ with open(SUM_PROJECTED_FILE, 'r') as f:
 			# Make a dictionary for each intermediate line to keep track of duplicates
 			track_dict = {}
 
-
 			print ("LINE NO: ", m_line_no)
 			flag_predicate = 0
 			
 
 			m_line_no+=1 # Increment line no. only when we encounter a blank token
-
+			x = 0 # Keep track of already numbers in dict.
 			for i, item in enumerate(intermediate_lines):
 				if i==0:
 					track_dict[i] = []
 					track_dict[i].append(i)
+					x = i
 
-				if intermediate_lines[i][1] == intermediate_lines[i-1][1]:
-					track_dict[i-1].append(i)
-				else:
+				if intermediate_lines[i][1] != intermediate_lines[i-1][1]:
+					x = i
 					track_dict[i] = []
 					track_dict[i].append(i)
-
+				elif intermediate_lines[i][1] == intermediate_lines[i-1][1]:
+					track_dict[x].append(i)
 
 			# Make an ordered dict.
 			od_track_dict = collections.OrderedDict(sorted(track_dict.items()))
 
-			token_no = 1
-			# Algorithm for merge here
-			for key, value in od_track_dict.items():
-				if len(value)==1:
-					projected_list.append(intermediate_lines[key])
-					
+			
+			pred_pos = {} #Dict for storing the position of the predicate, re-
 
+
+			token_no = 1
+			
+			# Algorithm for finding predicates and other related stuff
+			for key, value in od_track_dict.items():
+				for item in value:
+					if intermediate_lines[item][2] == 'Y':
+						pred_pos[key] = item
+
+			# Algorithm for compression
+			for key, value in od_track_dict.items():
+
+				if key not in pred_pos: # Normal tag
+					intermediate_lines[key][0] = str(token_no)
+					projected_list.append('\t'.join(x for i, x in enumerate(intermediate_lines[key])))
+				else:
+					intermediate_lines[key][0] = str(token_no)
+					projected_list.append('\t'.join(x for i, x in enumerate(intermediate_lines[pred_pos[key]])))
 				token_no+=1
 
 
-
-
-
-
-
-
-
-
-
-				#Check if predicate exists in any one of the item of the intermediate lines
-				if item[12] == 'Y':
-					flag_predicate = 1
-			'''
-			# If no predicate, skip the sentence altogether since it does not have 13 and 14 indexes
-			if flag_predicate == 0:
-				for i, item in enumerate(intermediate_lines):
-					string = '\t'.join(x for i, x in enumerate(item))
-					projected_list.append(string.strip())
-
-				projected_list.append('\n')
-				intermediate_lines = []
-				copy_intermediate_lines = []
-				continue
-			'''
-
-			copy_intermediate_lines = copy.deepcopy(intermediate_lines)
-			
-			for i,item in enumerate(copy_intermediate_lines):
-				string = '\t'.join(x for i, x in enumerate(item))
-				projected_list.append(string.strip())
-
 			projected_list.append('\n')
 			intermediate_lines = []
-			copy_intermediate_lines = []
 
 			continue
 
@@ -101,8 +85,9 @@ with open(SUM_PROJECTED_FILE, 'r') as f:
 
 
 # Store the fixed mate tools extended output
-with open('../../outputs/mate-parsing/un-normalised/extended_translated_eng_train_mate_unnorm.dat', 'w+') as f:
+with open('../../outputs/projected/compressed_projection.out', 'w+') as f:
 	for item in projected_list:
+		#print (item)
 		if item == '\n':
 			f.write(item)
 		else:
